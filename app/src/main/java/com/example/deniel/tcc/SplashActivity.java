4cd1dce8c4a7cbcb50a80f158baae34d0a845be0
denieldiniz <denieldiniz@gmail.com>
2015-10-23 12:56:21 -0300

    first commit

---
+++ app/src/main/java/com/example/deniel/tcc/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.deniel.tcc;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    @Override
+    public void run() {
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+}
