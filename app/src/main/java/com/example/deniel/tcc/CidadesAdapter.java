3c44adf1864c0597cec9e21eebd76e7146e09b2f
denieldiniz <denieldiniz@gmail.com>
2015-10-26 12:53:24 -0300

    first commit

---
+++ app/src/main/java/com/example/deniel/tcc/CidadesAdapter.java
@@ -0,0 +1,12 @@
+package com.example.deniel.tcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CidadesAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
