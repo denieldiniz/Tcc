127c2180a3ce8b89e294c673778d28a00fabfb00
denieldiniz <denieldiniz@gmail.com>
2015-11-04 22:29:32 -0300

    BuscaFragment + Parser

--- app/src/main/java/com/example/deniel/tcc/BuscaFragment.java
+++ app/src/main/java/com/example/deniel/tcc/BuscaFragment.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class BuscaFragment extends android.support.v4.app.Fragment {
+public class BuscaFragment extends android.support.v4.app.Fragment implements Runnable {
 
     public BuscaFragment() {
         // Required empty public constructor
@@ -21,7 +22,15 @@ public class BuscaFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busca, container, false);
+        //return inflater.inflate(R.layout.fragment_busca, container, false);
+        View view = inflater.inflate(R.layout.fragment_busca, container, false);
+        Button procurar = (Button) view.findViewById(R.id.procurar);
+
+        return view;
     }
 
+    @Override
+    public void run() {
+
+    }
 }
