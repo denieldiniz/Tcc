9cfb1fe5d620c5dd3abf5dc99ab108b54ade2d96
denieldiniz <denieldiniz@gmail.com>
2015-11-11 05:56:20 -0300

    BuscaFragment + Parser

--- app/src/main/java/com/example/deniel/tcc/BuscaFragment.java
+++ app/src/main/java/com/example/deniel/tcc/BuscaFragment.java
@@ -1,12 +1,18 @@
 package com.example.deniel.tcc;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class BuscaFragment extends android.support.v4.app.Fragment implements Runnable {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class BuscaFragment extends android.support.v4.app.Fragment implements Runnable, View.OnClickListener {
 
     public BuscaFragment() {
         // Required empty public constructor
@@ -25,6 +31,7 @@ public class BuscaFragment extends android.support.v4.app.Fragment implements Ru
         //return inflater.inflate(R.layout.fragment_busca, container, false);
         View view = inflater.inflate(R.layout.fragment_busca, container, false);
         Button procurar = (Button) view.findViewById(R.id.procurar);
+        procurar.setOnClickListener(this);
 
         return view;
     }
@@ -32,5 +39,28 @@ public class BuscaFragment extends android.support.v4.app.Fragment implements Ru
     @Override
     public void run() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JsonParser parser = new JsonParser();
+                    JSONObject obct = parser.getJSONFromUrl("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo");
+                    JSONArray array = obct.getJSONArray("geonames");
+                    ArrayList<JSONObject> list = new ArrayList<JSONObject>();
+                    for (int i = 0; i < array.length(); i++) {
+                        list.add(array.getJSONObject(i));
+                    }
+                    Log.v("tamanho", "tamanho:" + array.length());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
